9c24ff45ad3bcdcc406f498921e89f5a33064eed
maxliyo <...>
2025-11-04 11:08:25 +0500

    chatid ga ma'lumot qo'shildi

--- index.js
+++ index.js
@@ -1,20 +1,15 @@
 import TelegramBot from "node-telegram-bot-api";
 
-
 const token = "8348485669:AAEz-HH91LHcFNfxzP9h0kZLD0VzV9V38_8"; 
 
-
 const bot = new TelegramBot(token, { polling: true });
-
-
-
 bot.on("message", (msg) => {
   const text = msg.text;
   const chatId = msg.chat.id;
 
   console.log(`Yangi xabar: ${text} | Foydalanuvchi ID: ${chatId}`);
 
-  bot.sendMessage(chatId, `--> ${text}`);
+  bot.sendMessage(chatId, `Xush kelibsiz, Makhliyo👩‍💻`);
   
   bot.sendDice(chatId);
 });
